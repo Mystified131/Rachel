8cf256bf57c5fb27af09e063b2d893c287e586ed
Thomas Park <mystifiedthomas@gmail.com>
2021-03-02 21:10:47 -0600

    Tweaking

---
+++ BigDreamGenHopper.py
@@ -0,0 +1,67 @@
+import random
+import os
+from collections import defaultdict
+import datetime
+
+#this code retrieves the date and time from the computer, to create the timestamp
+
+right_now = datetime.datetime.now().isoformat()
+list = []
+
+for i in right_now:
+    if i.isnumeric():
+        list.append(i)
+
+time = ("".join(list))
+   
+srchstr = 'E:\\OriginalAudio\\Songs'
+
+contentdat = {}
+
+for subdir, dirs, files in os.walk(srchstr):
+    for file in files:
+        filepath = subdir + os.sep + file
+
+        if  filepath.endswith(".wav") or filepath.endswith(".mp3") :
+
+            tim = os.path.getmtime(filepath)
+
+            contentdat[filepath] = tim
+
+mapl = []
+
+newplyd = []
+
+for w in sorted(contentdat, key=contentdat.get, reverse=False):
+    mapl.append(w)
+    newplyd.append(contentdat[w])
+
+#hopl = [(0, -2), (0, -1), (0, 0), (0 , 1,), (0, 2), (1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (2, -2), (2, -1), (2, 0), (2, 1), (2, 2), (-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2), (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2)]
+
+hopl = []
+
+for obj in range(24):
+    a = random.randrange(-2, 2)
+    b = random.randrange(-2, 2)
+    hopl.append((a,b))
+
+moves = 50
+
+fst = random.randrange(len(mapl))
+
+outmap = []
+
+for ctr in range(moves):
+    hopran = random.randrange(len(hopl))
+    aval = (sum(int(i) for i in hopl[hopran])) * 4
+    fst += aval
+    astr = mapl[fst]
+    outmap.append(astr) 
+    mapl.remove(astr)
+    bighop = random.randrange(10)
+    if bighop > 7:
+        fst = random.randrange(len(mapl))
+
+print(outmap)
+
+
